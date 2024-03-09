262e2a5d3ca5138db68b32155a51069a0f754235
Tajusrs <148740286+Tajusrs@users.noreply.github.com>
2024-03-09 16:50:59 -0600

    Update TADEO_SOLIS_proyectoM2_palabra.py
    El programa le pide una palabra al usuario la cual debe encontrarse en un rango de 4 a 8 letras. Caso contrario repite el proceso.

--- TADEO_SOLIS_proyectoM2_palabra.py
+++ TADEO_SOLIS_proyectoM2_palabra.py
@@ -1,19 +1,25 @@
+# pedir datos al usuario
 while True:
     palabra = input('Por favor ingrese una palabra de entre 4 y 8 letras\n')
+    #condiciones de evaluacion de los datos ingresados por el usuario.
+    #Si el numero resulta mayor de lo estipulado.
     if len(palabra) > 8:
         num = len(palabra)
         print(f'La palabra contiene demasiadas letras. Son {num} letras en total.\n')
+    #si el dato resulta menor de lo estipulado.
     elif len (palabra) < 4:
         num = len(palabra)
         print(f'La palabra no contiene suficientes letras, solo tiene {num} letras.\n')
+    #cuando el dato cumple con las especificaciones.
     else:
+        #Corroborar que solo se hayan escrito letras en la palabra.
         for i in (palabra):
             if palabra.isdigit():
                 print('La palabra solo debe contener letras.\n')
                 break
             else:
                 continue
+        #Caso exitoso = fin del programa.
         print('La palabra es correcta.\n')
         break
     exit
-   
