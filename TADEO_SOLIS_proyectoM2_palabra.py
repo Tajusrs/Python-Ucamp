3be690081f3ac041988c23327ec52ec46cdfbb99
Tajusrs <148740286+Tajusrs@users.noreply.github.com>
2024-03-09 16:32:04 -0600

    Update TADEO_SOLIS_proyectoM2_palabra.py

--- TADEO_SOLIS_proyectoM2_palabra.py
+++ TADEO_SOLIS_proyectoM2_palabra.py
@@ -1,9 +1,11 @@
 while True:
     palabra = input('Por favor ingrese una palabra de entre 4 y 8 letras\n')
     if len(palabra) > 8:
-        print('La palabra contiene demasiadas letras.\n')
+        num = len(palabra)
+        print(f'La palabra contiene demasiadas letras. Son {num} letras en total.\n')
     elif len (palabra) < 4:
-        print('La palabra no contiene suficientes letras.\n')
+        num = len(palabra)
+        print(f'La palabra no contiene suficientes letras, solo tiene {num} letras.\n')
     else:
         for i in (palabra):
             if palabra.isdigit():
